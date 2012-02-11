82654bdbcdd6552415fd7c86458ea283ed7b0bab
Bosco So <rubymeetup@boscoso.com>
2012-02-10 17:43:21 -0800

    implemented UploadMgr#submit

--- spec/javascripts/upload_mgr_spec.js
+++ spec/javascripts/upload_mgr_spec.js
@@ -12,12 +12,48 @@ describe("UploadMgr", function() {
         mgr = new UploadMgr();
     });
 
+    afterEach(function () {
+        var q = new UploadQueue();
+        q.empty();
+    });
+
     it("should instantiate properly", function() {
         expect(mgr).toBeDefined();
     });
 
     it("exposes 'ttl' property", function() {
         expect(mgr.ttl).toBe(600000);
+    });
+
+    it("provides 'submit' method that queues to DB", function() {
+        var submitAndQuery = function() {
+            var dfd = $.Deferred();
+
+            mgr.submit(
+                "file:///tmp/dummy.jpg", "dummy1.jpg",
+                32.32, -120.12,
+                40, '{"device":666}'
+            ).
+                done(function(nRows) {
+
+                    var q = new UploadQueue();
+                    q.length().
+
+                        done(function(len) {
+                            dfd.resolve({returned: nRows, expected: len});
+                        });
+                });
+
+            return dfd.promise();
+        };
+
+        testPromise(
+            submitAndQuery(),
+            function(obj) {
+                expect(obj.returned).toBe(1);
+                expect(obj.expected).toBe(1);
+            }
+        );
     });
 
 });
